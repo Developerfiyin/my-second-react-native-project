8afb8484fdceaaa3e1013a796f70f64a9fbcef07
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2026-02-03 17:06:41 +0100

    updated layout.tsx

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,10 +1,49 @@
+import AntDesign from "@expo/vector-icons/AntDesign";
+import EvilIcons from "@expo/vector-icons/EvilIcons";
+import Feather from "@expo/vector-icons/Feather";
+import SimpleLineIcons from "@expo/vector-icons/SimpleLineIcons";
 import { Tabs } from "expo-router";
 
 export default function TabsLayout() {
   return (
-    <Tabs screenOptions={{ tabBarInactiveTintColor: "blue" }}>
-      <Tabs.Screen name="index" options={{ title: "Home" }} />
-      <Tabs.Screen name="Home" options={{ title: "Home Screen" }} />
+    <Tabs
+      screenOptions={{
+        tabBarInactiveTintColor: "darkgray",
+        tabBarStyle: {
+          backgroundColor: "white",
+          height: 110,
+          paddingBottom: 20,
+          paddingTop: 15,
+        },
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Home",
+          tabBarIcon: ({ color, focused }) => {
+            return focused ? (
+              <Feather name="home" size={35} color="darkgrey" />
+            ) : (
+              <AntDesign name="home" size={35} color="black" />
+            );
+          },
+        }}
+      />
+      <Tabs.Screen
+        name="Map"
+        options={{
+          title: "Map",
+          tabBarIcon: ({ color, focused }) => {
+            return focused ? (
+              <SimpleLineIcons name="location-pin" size={24} color="darkgrey" />
+            ) : (
+              <EvilIcons name="location" size={35} color="black" />
+            );
+          },
+          tabBarLabelStyle: { fontSize: 14, color: "black" },
+        }}
+      />
     </Tabs>
   );
 }
