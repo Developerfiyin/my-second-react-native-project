f815a9c89d52ad4191601c140f7949b52880645a
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2026-02-03 16:09:21 +0100

    Updated Layout.tsx

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -2,7 +2,7 @@ import { Tabs } from "expo-router";
 
 export default function TabsLayout() {
   return (
-    <Tabs>
+    <Tabs screenOptions={{ tabBarInactiveTintColor: "blue" }}>
       <Tabs.Screen name="index" options={{ title: "Home" }} />
       <Tabs.Screen name="Home" options={{ title: "Home Screen" }} />
     </Tabs>
