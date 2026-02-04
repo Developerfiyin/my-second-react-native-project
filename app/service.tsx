2c6967205e0115a5834dc52e723555981fa93b9a
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2026-02-04 18:28:28 +0100

    duplicate the last commit  4 times

--- app/service.tsx
+++ app/service.tsx
@@ -69,9 +69,160 @@ export default function ServiceScreen() {
           </Text>
         </View>
       </View>
+
+      <View>
+        <View
+          style={{
+            backgroundColor: "#E8E8E8",
+            display: "flex",
+            justifyContent: "space-around",
+            flexDirection: "row",
+            padding: 13,
+            borderRadius: 10,
+            borderColor: "#ffff",
+            marginTop: 20,
+            marginHorizontal: 20,
+          }}
+        >
+          <MaterialCommunityIcons
+            name="car-wash"
+            size={40}
+            color="black"
+            style={styles.icon}
+          />
+          <View
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              marginRight: 50,
+            }}
+          >
+            <Text style={{ fontSize: 20, fontWeight: "600" }}>
+              Puncture Repair
+            </Text>
+            <Text style={{ color: "#0F172A" }}> Car Wash</Text>
+          </View>
+          <Text style={{ fontSize: 30, color: "#0F172A", fontWeight: "500" }}>
+            $56
+          </Text>
+        </View>
+      </View>
+      <View>
+        <View
+          style={{
+            backgroundColor: "#E8E8E8",
+            display: "flex",
+            justifyContent: "space-around",
+            flexDirection: "row",
+            padding: 13,
+            borderRadius: 10,
+            borderColor: "#ffff",
+            marginTop: 20,
+            marginHorizontal: 20,
+          }}
+        >
+          <MaterialCommunityIcons
+            name="car-wash"
+            size={40}
+            color="black"
+            style={styles.icon}
+          />
+          <View
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              marginRight: 50,
+            }}
+          >
+            <Text style={{ fontSize: 20, fontWeight: "600" }}>
+              Puncture Repair
+            </Text>
+            <Text style={{ color: "#0F172A" }}> Car Wash</Text>
+          </View>
+          <Text style={{ fontSize: 30, color: "#0F172A", fontWeight: "500" }}>
+            $56
+          </Text>
+        </View>
+      </View>
+      <View>
+        <View
+          style={{
+            backgroundColor: "#E8E8E8",
+            display: "flex",
+            justifyContent: "space-around",
+            flexDirection: "row",
+            padding: 13,
+            borderRadius: 10,
+            borderColor: "#ffff",
+            marginTop: 20,
+            marginHorizontal: 20,
+          }}
+        >
+          <MaterialCommunityIcons
+            name="car-wash"
+            size={40}
+            color="black"
+            style={styles.icon}
+          />
+          <View
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              marginRight: 50,
+            }}
+          >
+            <Text style={{ fontSize: 20, fontWeight: "600" }}>
+              Puncture Repair
+            </Text>
+            <Text style={{ color: "#0F172A" }}> Car Wash</Text>
+          </View>
+          <Text style={{ fontSize: 30, color: "#0F172A", fontWeight: "500" }}>
+            $56
+          </Text>
+        </View>
+      </View>
+
+      <View>
+        <View
+          style={{
+            backgroundColor: "#E8E8E8",
+            display: "flex",
+            justifyContent: "space-around",
+            flexDirection: "row",
+            padding: 13,
+            borderRadius: 10,
+            borderColor: "#ffff",
+            marginTop: 20,
+            marginHorizontal: 20,
+          }}
+        >
+          <MaterialCommunityIcons
+            name="car-wash"
+            size={40}
+            color="black"
+            style={styles.icon}
+          />
+          <View
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              marginRight: 50,
+            }}
+          >
+            <Text style={{ fontSize: 20, fontWeight: "600" }}>
+              Puncture Repair
+            </Text>
+            <Text style={{ color: "#0F172A" }}> Car Wash</Text>
+          </View>
+          <Text style={{ fontSize: 30, color: "#0F172A", fontWeight: "500" }}>
+            $56
+          </Text>
+        </View>
+      </View>
     </View>
   );
 }
+
 const styles = StyleSheet.create({
   icon: {
     backgroundColor: "#bcc9e6",
