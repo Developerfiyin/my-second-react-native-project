be88d96aba44be8f2dae8165df863ecaeeee8939
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2026-02-04 23:01:43 +0100

    Styled vehicle.tsx container

--- app/vehicle.tsx
+++ app/vehicle.tsx
@@ -1,5 +1,23 @@
-import { View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function VehicleDetails() {
-  return <View></View>;
+  return (
+    <View
+      style={{
+        flexDirection: "column",
+        marginTop: 20,
+        justifyContent: "center",
+      }}
+    >
+      <Text style={{ textAlign: "center", fontSize: 18, color: "#0F172A" }}>
+        Help us match the right tires for your car
+      </Text>
+    </View>
+  );
 }
+const styles = StyleSheet.create({
+  text: {
+    textAlign: "center",
+    fontSize: 18,
+  },
+});
