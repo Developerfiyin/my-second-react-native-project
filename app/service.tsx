a1a8cba859bafd6abb84471e3459b36abd66a2d8
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2026-02-04 18:26:10 +0100

    Added a view ta containing an icon an some text

--- app/service.tsx
+++ app/service.tsx
@@ -1,6 +1,8 @@
 import FontAwesome5 from "@expo/vector-icons/FontAwesome5";
+import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
+
 import { Link } from "expo-router";
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 export default function ServiceScreen() {
   return (
     <View style={{ display: "flex", flexDirection: "column" }}>
@@ -29,6 +31,52 @@ export default function ServiceScreen() {
       >
         Receive all manner of services from the comfort of your home.
       </Text>
+
+      <View>
+        <View
+          style={{
+            backgroundColor: "#E8E8E8",
+            display: "flex",
+            justifyContent: "space-around",
+            flexDirection: "row",
+            padding: 13,
+            borderRadius: 10,
+            borderColor: "#ffff",
+            marginTop: 20,
+            marginHorizontal: 20,
+          }}
+        >
+          <MaterialCommunityIcons
+            name="car-wash"
+            size={40}
+            color="black"
+            style={styles.icon}
+          />
+          <View
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              marginRight: 50,
+            }}
+          >
+            <Text style={{ fontSize: 20, fontWeight: "600" }}>
+              Puncture Repair
+            </Text>
+            <Text style={{ color: "#0F172A" }}> Car Wash</Text>
+          </View>
+          <Text style={{ fontSize: 30, color: "#0F172A", fontWeight: "500" }}>
+            $56
+          </Text>
+        </View>
+      </View>
     </View>
   );
 }
+const styles = StyleSheet.create({
+  icon: {
+    backgroundColor: "#bcc9e6",
+    padding: 20,
+    borderRadius: 15,
+    alignSelf: "center",
+  },
+});
