fae64623834c3ac155b0383242410d942165d768
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2026-02-04 23:29:01 +0100

    For the vehicle screen, i added an input and text inside a view tag

--- app/vehicle.tsx
+++ app/vehicle.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function VehicleDetails() {
   return (
@@ -12,12 +12,32 @@ export default function VehicleDetails() {
       <Text style={{ textAlign: "center", fontSize: 18, color: "#0F172A" }}>
         Help us match the right tires for your car
       </Text>
+
+      <View style={styles.View}>
+        <Text style={styles.text}>MAKE </Text>
+        <TextInput placeholder="e.g Tesla" style={styles.input}></TextInput>
+      </View>
     </View>
   );
 }
 const styles = StyleSheet.create({
   text: {
-    textAlign: "center",
     fontSize: 18,
+    marginHorizontal: 20,
+    color: "grey",
+    fontWeight: "400",
+  },
+  View: {
+    marginHorizontal: 20,
+    gap: 10,
+    marginVertical: 25,
+  },
+  input: {
+    marginHorizontal: 20,
+    borderRadius: 10,
+    backgroundColor: "#E8E8E8",
+    paddingLeft: 20,
+    paddingVertical: "30%",
+    padding: 20,
   },
 });
