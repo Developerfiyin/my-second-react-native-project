258e72b1ffa5a938253909e38ebc0d8b571d03b5
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2026-02-06 22:10:12 +0100

    Added a view tag and a text inside it

--- app/account.tsx
+++ app/account.tsx
@@ -1,4 +1,27 @@
-import { View } from "react-native";
+import { Image, StyleSheet, Text, View } from "react-native";
+
 export default function AccountScreen() {
-  return <View></View>;
+  return (
+    <View style={{ flexDirection: "column", gap: 20 }}>
+      <Image
+        source={require("../assets/images/image 32.png")}
+        style={{ marginHorizontal: "auto", marginTop: 50 }}
+      />
+
+      <View style={styles.View}>
+        <Text style={{ color: "grey", fontSize: 18 }}> Username</Text>
+        <Text style={{ color: "#0F172A", fontSize: 20 }}>Emeka Orji</Text>
+      </View>
+    </View>
+  );
 }
+const styles = StyleSheet.create({
+  View: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    borderBottomColor: "lightgrey",
+    paddingBottom: "6%",
+    borderBottomWidth: 2,
+    marginHorizontal: 15,
+  },
+});
