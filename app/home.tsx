30261608dfd30009757e8be88f4c08e4f6c6b412
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2026-02-07 16:24:03 +0100

    Struggled with making splash the first screen

--- app/home.tsx
+++ app/home.tsx
@@ -1,12 +1,15 @@
+import { router } from "expo-router";
 import { Image, Pressable, StyleSheet, Text, View } from "react-native";
-
 export default function HomeScreen() {
   return (
     <View>
       <Image source={require("../assets/images/image 4.png")} style={{}} />
       <Text> Get the best service needed for your car here</Text>
 
-      <Pressable style={styles.pressable}>
+      <Pressable
+        style={styles.pressable}
+        onPress={() => router.push("/description")}
+      >
         <Text style={styles.text1}> Continue</Text>
       </Pressable>
     </View>
