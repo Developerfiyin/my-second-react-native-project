36ae4157d305bb8b5d186c8fc99b96bfda0dd279
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2026-02-07 16:44:03 +0100

    Sucessfuly made splash.tsx file the first screen

--- app/splash.tsx
+++ app/splash.tsx
@@ -8,22 +8,20 @@ export default function Splash() {
   useEffect(() => {
     const timer = setTimeout(() => {
       router.replace("/home");
-    }, 4000);
+    }, 6000);
 
     return () => clearTimeout(timer);
   }, []);
 
   return (
     <View style={styles.container}>
-      <Image
-        source={require("../assets/images/image.png")}
-        style={styles.image}
-      />
-
       <Text style={styles.text}>
-        {" "}
         Get the best service needed for your car here
       </Text>
+      <Image
+        source={require("../assets/images/first-screen.png")}
+        style={styles.image}
+      />
     </View>
   );
 }
@@ -31,18 +29,27 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#0f1729",
-    alignItems: "center",
-    justifyContent: "center",
   },
   image: {
-    width: 120,
-    height: 120,
+    width: 392,
+    height: 479,
+    opacity: 1,
+    top: 87,
+    marginLeft: "auto",
     resizeMode: "contain",
-    marginBottom: 16,
   },
   text: {
     color: "#fff",
-    fontSize: 18,
+    fontSize: 38,
     fontWeight: "600",
+    marginTop: 40,
+    marginHorizontal: 20,
   },
 });
+
+{
+  /* height: 320,
+    resizeMode: "contain",
+    marginTop: 50,
+    marginLeft: "auto",*/
+}
