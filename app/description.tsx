2fcecdd51494fef0ebf508776c2f8c5c4c754071
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2026-02-07 11:21:56 +0100

    Added a view tag containing an image

--- app/description.tsx
+++ app/description.tsx
@@ -1,4 +1,19 @@
-import { View } from "react-native";
+import { Image, StyleSheet, View } from "react-native";
 export default function Description() {
-  return <View></View>;
+  return (
+    <View style={{ flexDirection: "column", gap: "10" }}>
+      <View style={{ backgroundColor: "#E8E8E8" }}>
+        <Image
+          source={require("../assets/images/image 20.png")}
+          style={styles.image}
+        />
+      </View>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  image: {
+    alignSelf: "center",
+  },
+});
