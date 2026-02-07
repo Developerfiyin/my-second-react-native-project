29fc343f076d5a70d05b286522af42bb7cfe4316
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2026-02-07 17:08:42 +0100

    Added a pressable tag that willlead to the sign up screen

--- app/home.tsx
+++ app/home.tsx
@@ -2,15 +2,41 @@ import { router } from "expo-router";
 import { Image, Pressable, StyleSheet, Text, View } from "react-native";
 export default function HomeScreen() {
   return (
-    <View>
-      <Image source={require("../assets/images/image 4.png")} style={{}} />
-      <Text> Get the best service needed for your car here</Text>
+    <View
+      style={{
+        flexDirection: "column",
+        gap: 6,
+      }}
+    >
+      <View>
+        <Image
+          source={require("../assets/images/image 4.png")}
+          style={{ marginHorizontal: "auto", top: "auto", marginTop: 80 }}
+        />
+        <Image
+          source={require("../assets/images/Ellipse 1.png")}
+          style={{ marginHorizontal: "auto", bottom: 100 }}
+        />
+      </View>
+
+      <Text
+        style={{
+          fontWeight: "600",
+          color: "#0f172a",
+          fontSize: 32,
+          textAlign: "center",
+          marginBottom: 20,
+          marginHorizontal: "10%",
+        }}
+      >
+        Get the best service needed for your car here
+      </Text>
 
       <Pressable
         style={styles.pressable}
         onPress={() => router.push("/description")}
       >
-        <Text style={styles.text1}> Continue</Text>
+        <Text style={styles.text1}> Get Started</Text>
       </Pressable>
     </View>
   );
@@ -19,7 +45,7 @@ export default function HomeScreen() {
 const styles = StyleSheet.create({
   pressable: {
     backgroundColor: "#0f172a",
-
+    marginTop: "10%",
     padding: 20,
     borderRadius: 10,
     marginHorizontal: 20,
