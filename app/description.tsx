e7dea1a46afe48a29b83294c3589347ba298eef5
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2026-02-07 12:09:12 +0100

    styled the view

--- app/description.tsx
+++ app/description.tsx
@@ -2,18 +2,46 @@ import { Image, StyleSheet, View } from "react-native";
 export default function Description() {
   return (
     <View style={{ flexDirection: "column", gap: "10" }}>
-      <View style={{ backgroundColor: "#E8E8E8" }}>
+      <View style={styles.container}>
         <Image
           source={require("../assets/images/image 20.png")}
           style={styles.image}
         />
+        <View style={styles.dotsContainer}>
+          <View style={styles.activeDot}></View>
+          <View style={styles.dot}></View>
+          <View style={styles.dot}></View>
+          <View style={styles.dot}></View>
+          <View style={styles.dot}></View>
+        </View>
       </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  container: {
+    alignItems: "center", // centers dots
+  },
+
   image: {
-    alignSelf: "center",
+    borderRadius: 16,
+  },
+
+  dotsContainer: {
+    flexDirection: "row",
+    marginTop: 8,
+    gap: 8,
+  },
+
+  dot: {
+    width: 8,
+    height: 8,
+    borderRadius: 4,
+    backgroundColor: "red",
+  },
+
+  activeDot: {
+    backgroundColor: "#2563EB",
   },
 });
